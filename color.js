e26b991ef352a1442f45658e4c3d809dd6791f1c
KaydenDou <kdouangboupha1@gmail.com>
2023-08-20 15:58:36 -0500

    ALOT

---
+++ color.js
@@ -0,0 +1,19 @@
+const observers = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('change');
+            document.body.style.backgroundColor = '#2a2a2a';
+            document.getElementById('first').style.backgroundColor = '#2a2a2a';
+            document.getElementById('third').style.backgroundColor = '#2a2a2a';
+        } else {
+            entry.target.classList.remove('change');
+            document.body.style.backgroundColor = '#fdfdfd';
+            document.getElementById('first').style.backgroundColor = '#fdfdfd';
+            document.getElementById('third').style.backgroundColor = '#fdfdfd';
+        }
+    });
+});
+
+const color = document.querySelectorAll('.color');
+color.forEach((el) => observers.observe(el));
