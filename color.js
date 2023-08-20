021de3a3253abc476d44be314bfa5df095710b64
KaydenDou <kdouangboupha1@gmail.com>
2023-08-20 17:37:52 -0500

    some finishing touches

--- color.js
+++ color.js
@@ -4,12 +4,12 @@ const observers = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('change');
             document.body.style.backgroundColor = '#2a2a2a';
-            document.getElementById('first').style.backgroundColor = '#2a2a2a';
+            document.getElementById('tint').style.backgroundColor = '#2a2a2a';
             document.getElementById('third').style.backgroundColor = '#2a2a2a';
         } else {
             entry.target.classList.remove('change');
             document.body.style.backgroundColor = '#fdfdfd';
-            document.getElementById('first').style.backgroundColor = '#fdfdfd';
+            document.getElementById('tint').style.backgroundColor = '#fdfdfd';
             document.getElementById('third').style.backgroundColor = '#fdfdfd';
         }
     });
